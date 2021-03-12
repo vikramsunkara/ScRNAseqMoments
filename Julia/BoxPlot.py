517ea248e0016ca410dc60268ec1d5f5e1f64c68
AlexiaNomena <missralexia1@gmail.com>
2021-03-12 22:11:39 +0100

    changed

--- Julia/BoxPlot.py
+++ Julia/BoxPlot.py
@@ -11,6 +11,55 @@ pdf= PdfPages("Figures/MI_sensitivity"+".pdf")
 data0 = pd.read_csv("MI_10000traj_shift30.csv")
 data0.head()
 No_Up = data0["No_Up"]
+Single_Up = data0["Single_Up"]
+Double_Up = data0["Double_Up"]
+
+#### For not change first #######
+
+matplotlib.rc('xtick', labelsize=20) 
+matplotlib.rc('ytick', labelsize=20) 
+fig = plt.figure(figsize=(7,7))
+ax = fig.add_subplot(111)
+
+models = [No_Up, Single_Up, Double_Up]
+ax.boxplot(models,showfliers=False) # showfliers = False remove outliers
+plt.xticks([1, 2, 3], ['No-I', 'Mono-I', 'Bi-I'])
+xmin, xmax = ax.get_xlim()
+plt.plot(np.linspace(xmin, xmax, 100), np.percentile(No_Up, 95)*np.ones(100), "--", color = "black", linewidth = 2)  
+plt.text(2.25, 1.1*np.percentile(No_Up, 95), "95th percentile (No-I)", fontsize = 15)
+plt.ylabel("MI scores", fontsize = 20)
+
+# One way anova https://reneshbedre.github.io/blog/anova.html
+import scipy.stats as stats
+# stats f_oneway functions takes the groups as input and returns F and P-value
+fvalue, pvalue = stats.f_oneway(data0['No_Up'], data0['Single_Up'], data0['Double_Up'])
+print("One way anova")
+print("Fvalue =", fvalue, "pvalue", pvalue)
+print("######################################")
+#### get an R like output
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+# reshape the d dataframe suitable for statsmodels package 
+d_melt = pd.melt(data0.reset_index(), id_vars=['index'], value_vars=['No_Up', 'Single_Up', 'Double_Up'])
+# replace column names
+d_melt.columns = ['index', 'treatments', 'value']
+# Ordinary Least Squares (OLS) model
+model = ols('value ~ C(treatments)', data=d_melt).fit()
+anova_table = sm.stats.anova_lm(model, typ=2)
+anova_table
+
+# pairwise comparision significance with HSD https://reneshbedre.github.io/blog/anova.html
+from pingouin import pairwise_tukey
+# perform multiple pairwise comparison (Tukey HSD)
+# for unbalanced (unequal sample size) data, pairwise_tukey uses Tukey-Kramer test
+print("Pairwise comparison with Tukey HSD")
+m_comp = pairwise_tukey(data=d_melt, dv='value', between='treatments')
+print(m_comp)
+
+plt.title("Regulation: no change", fontsize = 16)
+plt.tight_layout()
+pdf.savefig(fig)
+##### For the runs with changes ###################
 
 sigma_1 = 0.01875
 sigma_1_list = sigma_1*np.array([1/2, 2, 2**2, 2**3, 2**4])
@@ -42,7 +91,7 @@ for i in range(len(sigma_1_list)):
     plt.xticks([1, 2, 3, 4], ['No-I', 'Mono-I', 'Bi-I', 'Bi-I-asym'])
     xmin, xmax = ax.get_xlim()
     plt.plot(np.linspace(xmin, xmax, 100), np.percentile(No_Up, 95)*np.ones(100), "--", color = "black", linewidth = 2)  
-    plt.text(2.25, 1.1*np.percentile(No_Up, 95), "95th percentile (No-I)", fontsize = 15)
+    plt.text(2.75, 1.1*np.percentile(No_Up, 95), "95th percentile (No-I)", fontsize = 15)
     plt.ylabel("MI scores", fontsize = 20)
     
     # One way anova https://reneshbedre.github.io/blog/anova.html
